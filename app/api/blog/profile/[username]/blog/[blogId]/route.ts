92ed08cd2537d5c1ecf9eee5ea404db01d153597
Ayan <aqiindia.a2mation@gmail.com>
2026-01-30 12:46:03 +0530

    feat(blog-api): update routes and add get single blog endpoint

---
+++ app/api/blog/profile/[username]/blog/[blogId]/route.ts
@@ -0,0 +1,95 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+// Get a single post
+export async function GET(
+    req: NextRequest,
+    params: {
+        params: Promise<{ blogId: string }>
+    }) {
+    try {
+
+        const { blogId } = await params.params;
+
+        if (!blogId) {
+            return NextResponse.json({ message: "POST ID not found" }, { status: 403 });
+        }
+
+        const post = await prisma.blogPost.findUnique({
+            where: { id: blogId },
+            include: {
+                author: true,
+                comments: true,
+                
+            }
+        });
+
+        if (!post) {
+            return NextResponse.json({ message: "Blog not found" }, { status: 403 });
+        }
+
+        return NextResponse.json(post);
+
+    } catch (error) {
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
+
+// Delete a Post
+export async function DELETE(
+    req: NextRequest,
+    params: { params: Promise<{ blogId: string, profileId: string }> }
+) {
+    try {
+        const { blogId, profileId } = await params.params;
+
+
+        if (!blogId) {
+            return NextResponse.json({ message: "POST ID not found" }, { status: 401 });
+        }
+
+        // TODO:: ADD AUTHENTICATION
+
+        const post = await prisma.blogPost.findUnique({
+            where: { id: blogId },
+            select: { id: true, authorId: true },
+        });
+
+        if (!post) {
+            return NextResponse.json(
+                { message: "Post not found" },
+                { status: 404 }
+            );
+        }
+
+
+        await prisma.$transaction([
+            prisma.blogPostCategory.deleteMany({
+                where: {
+                    postId: post.id,
+                },
+            }),
+
+            prisma.blogPost.delete({
+                where: {
+                    id: post.id,
+                },
+            }),
+        ]);
+
+
+        return NextResponse.json(
+            { message: "Post deleted successfully" },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("DELETE POST ERROR:", error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
