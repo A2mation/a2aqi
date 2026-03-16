e62795105f92899096628febbd6bc28c6569d100
Ayan <aqiindia.a2mation@gmail.com>
2026-03-16 17:27:55 +0530

    feat(monitor-ui): setup monitor home page layout

---
+++ app/(ui)/monitor/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import MonitorLoginFrom from '../components/monitor-login-form'
+
+const MonitorSignUpPage = () => {
+    return (
+        <section className='min-h-screen'>
+            <div className='flex items-center justify-center h-screen'>
+                <MonitorLoginFrom />
+            </div>
+        </section>
+    )
+}
+
+export default MonitorSignUpPage
