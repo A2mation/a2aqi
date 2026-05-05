61c38cab0b028f9aa17e316140c698ebd69d1c88
Ayan <aqiindia.a2mation@gmail.com>
2026-05-05 09:15:36 +0530

    fix: add turbopackIgnore comment to fullPath resolution in getBase64Image function

--- helpers/convert-base64-image.ts
+++ helpers/convert-base64-image.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 
 export const getBase64Image = (filePath: string): string => {
     try {
-        const fullPath = path.resolve(process.cwd(), filePath);
+        const fullPath = path.resolve(/*turbopackIgnore: true*/process.cwd(), filePath);
         const image = fs.readFileSync(fullPath);
         const extension = path.extname(filePath).replace('.', '');
         return `data:image/${extension};base64,${image.toString('base64')}`;
