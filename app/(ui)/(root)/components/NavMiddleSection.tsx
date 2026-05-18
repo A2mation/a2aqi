a0d681bfcee9ff66b4acc16d999c5f15cf373094
Ayan <aqiindia.a2mation@gmail.com>
2026-05-18 16:45:39 +0530

    feat: enhance NavbarMiddleSection with dropdown menu for sector selection and update click action prop

--- app/(ui)/(root)/components/NavMiddleSection.tsx
+++ app/(ui)/(root)/components/NavMiddleSection.tsx
@@ -1,18 +1,32 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-import { Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { IconBuildingBank, IconBuildingFactory2, IconBuildingHospital, IconBuildings } from "@tabler/icons-react";
+
 
 export function NavbarMiddleSection({
   className,
-  onClick
+  onClickAction
 }: {
   className?: string;
-  onClick?: () => void;
+  onClickAction?: () => void;
 }) {
-  const [active, setActive] = useState<string | null>(null);
+  const router = useRouter();
+  const [active, setActive] = useState < string | null > (null);
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -35,7 +49,7 @@ export function NavbarMiddleSection({
             "group flex flex-col italic gap-0.5 cursor-pointer text-lg md:text-xl font-bold  dark:text-white",
             isScrolled ? "text-black" : "text-black"
           )}
-          onClick={onClick}
+          onClick={onClickAction}
         >
           Learn
           <div
@@ -52,7 +66,7 @@ export function NavbarMiddleSection({
             "group flex flex-col italic gap-0.5 cursor-pointer text-lg md:text-xl font-bold dark:text-white",
             isScrolled ? "text-black" : "text-black"
           )}
-          onClick={onClick}
+          onClick={onClickAction}
         >
           Blogs
           <div
@@ -71,7 +85,7 @@ export function NavbarMiddleSection({
             "group relative flex flex-col italic gap-0.5 cursor-pointer text-lg md:text-xl font-black transition-all duration-300",
             isScrolled ? "text-slate-900" : "text-white"
           )}
-          onClick={onClick}
+          onClick={onClickAction}
         >
           {/* The Animated Aurora Text */}
           <span className="relative z-10 transition-all duration-500 text-transparent bg-clip-text bg-linear-to-r from-blue-400 via-cyan-300 to-emerald-400 animate-aurora-text">
@@ -87,35 +101,49 @@ export function NavbarMiddleSection({
           />
         </Link>
 
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button
+              className={cn(
+                "group flex flex-col items-start italic gap-0.5 cursor-pointer text-lg md:text-xl font-bold  dark:text-white border-0",
+                isScrolled ? "text-black" : "text-black"
+              )}>
+              Sectors
+              <div
+                className={cn(
+                  "h-0.5 w-0 group-hover:w-full transition-all duration-500 ease-out",
+                  "bg-linear-to-r from-blue-500 via-cyan-400 to-emerald-500 shadow-[0_1px_8px_rgba(59,130,246,0.4)]"
+                )}
+              />
+            </button>
+          </DropdownMenuTrigger>
 
-        <MenuItem setActive={setActive} active={active} item="Sectors">
-          <div className="text-sm grid grid-cols-1 md:grid-cols-2 gap-5 p-4">
-            <ProductItem
-              title="Construction"
-              href="/air-quality-monitoring-construction-sites"
-              src="https://motionarray.imgix.net/2519128-LHNmiJT7pZ-high_0005.jpg?w=660&q=60&fit=max&auto=format"
-              onclick={onClick}
-            />
-            <ProductItem
-              title="Hospitals"
-              href="/air-quality-monitoring-hospital-sites"
-              src="https://elements-resized.envatousercontent.com/elements-video-cover-images/bbd0ce1a-28f3-4c0d-bd14-104264c92b10/video_preview/video_preview_0000.jpg?w=1200&h=630&cf_fit=crop&q=85&format=jpeg&s=5e568852b985ae253c51752c8d3e2c772bf55c5bf96a1b04d930d99cceb5bb9f"
-              onclick={onClick}
-            />
-            <ProductItem
-              title="Smart Cities"
-              href="/air-quality-monitoring-smart-cities"
-              src="https://www.shutterstock.com/image-vector/smart-city-graphic-one-blue-260nw-2476922281.jpg"
-              onclick={onClick}
-            />
-            <ProductItem
-              title="Institution"
-              href="/air-quality-monitoring-industries"
-              src="https://static.vecteezy.com/system/resources/thumbnails/022/530/575/small/school-building-exterior-vector-illustration-png.png"
-              onclick={onClick}
-            />
-          </div>
-        </MenuItem>
+          <DropdownMenuContent className="w-50" align="start">
+            <DropdownMenuGroup>
+              <DropdownMenuLabel>
+                Sectors
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => router.push('/air-quality-monitoring-construction-sites')}>
+                Construction
+                <DropdownMenuShortcut><IconBuildingFactory2 stroke={2} /></DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push('/air-quality-monitoring-hospital-sites')}>
+                Hospitals
+                <DropdownMenuShortcut><IconBuildingHospital stroke={2} /></DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push('/air-quality-monitoring-smart-cities')}>
+                Smart Cities
+                <DropdownMenuShortcut><IconBuildings stroke={2} /></DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push('/air-quality-monitoring-industries')}>
+                Institution
+                <DropdownMenuShortcut><IconBuildingBank stroke={2} /></DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+
+        </DropdownMenu>
 
         <Link
           href={"/about-us"}
@@ -123,16 +151,16 @@ export function NavbarMiddleSection({
             "group flex flex-col italic gap-0.5 cursor-pointer text-lg md:text-xl font-bold  dark:text-white",
             isScrolled ? "text-black" : "text-black"
           )}
-          onClick={onClick}
+          onClick={onClickAction}
         >
-            <div className="flex flex-row gap-1">
-              <span>
-                About
-              </span>
-              <span>
-                Us
-              </span>
-            </div>
+          <div className="flex flex-row gap-1">
+            <span>
+              About
+            </span>
+            <span>
+              Us
+            </span>
+          </div>
           <div
             className={cn(
               "h-0.5 w-0 group-hover:w-full transition-all duration-500 ease-out",
